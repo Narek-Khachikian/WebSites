a3648cab50321a0a8d9274ee3b7962754758bc9d
Narek Khachikian <narekbarev@hotmail.com>
2020-07-12 17:14:40 +0400

    Add project files.

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+        public int PageSize = 4;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+
+        public IActionResult Index(string category, int productPage = 1) =>
+            View(new ProductsListViewModel()
+            {
+                Products = !(category =="" || category==null) ? repository.Products.Where(p=> p.Category==null || p.Category == category).
+                OrderBy(p => p.ProductId).Skip((productPage - 1) * PageSize).
+                Take(PageSize): repository.Products.OrderBy(p=>p.ProductId).Skip((productPage-1)*PageSize).Take(PageSize) ,
+                PagingInfo = new PagingInfo()
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = category == null? repository.Products.Count() : repository.Products.Where(p=>p.Category == category).Count()
+                }
+            });
+    }
+}
---
+++ SportsStore/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        public long ProductId { get; set; }
+
+        [Required(ErrorMessage ="Please enter a product name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please enter a description")]
+        public string Description { get; set; }
+
+        [Column(TypeName ="decimal(8,2)"), Required(ErrorMessage ="A decimal price is required"), 
+            Range(0.01,double.MaxValue, ErrorMessage ="Please enter a positive integer")]
+        public decimal? Price { get; set; }
+
+        [Required(ErrorMessage ="Please specify a category")]
+        public string Category { get; set; }
+    }
+}
