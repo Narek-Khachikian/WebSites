d7fafc8a5b66be9b003b05ae089a656ca469bc35
Narek Khachikian <narekbarev@hotmail.com>
2020-07-14 01:03:27 +0400

    Security of Adminstration is Added

---
+++ SportsStore/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SportsStore.Models.ViewModels
+{
+    public class LoginModel
+    {
+        [Required]
+        //[RegularExpression("^[a-zA-Z0-9].{4,126}$",ErrorMessage = "Patern must be ^[a-zA-Z0-9].{4,126}$")]
+        public string Name { get; set; }
+        [Required]
+        //[RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$% ^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$",
+        //    ErrorMessage ="at least a digit, a small leter, a capital leter, a symbol and 8-32 characters in total")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace SportsStore
 {
@@ -37,6 +38,8 @@ namespace SportsStore
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddServerSideBlazor();
+            services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration["Connection Strings:IdentityConnection"]));
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +50,8 @@ namespace SportsStore
             app.UseStaticFiles();
             app.UseSession();
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
@@ -60,6 +65,7 @@ namespace SportsStore
                 endpoints.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
             });
             SeedData.EnsurePopulated(app);
+            IdentitySeedData.EnsurePopulated(app);
             
 
         }
