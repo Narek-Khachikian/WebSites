5ab90a69ec34155cb5e495c1db8dafe5034b3833
Narek Khachikian <narekbarev@hotmail.com>
2020-07-24 00:39:39 +0400

    End of Error Handling

--- Platform/Startup.cs
+++ Platform/Startup.cs
@@ -41,6 +41,18 @@ namespace Platform
                 option.Cookie.IsEssential = true;
             });
 
+            services.AddHsts(options =>
+            {
+                options.MaxAge = TimeSpan.FromDays(1);
+                options.IncludeSubDomains = true;
+            });
+
+            //services.AddHttpsRedirection(opt =>
+            //{
+            //    opt.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+            //    opt.HttpsPort = 44399;
+            //});
+
             //services.Configure<MessageOptions>(Config.GetSection("Location"));
             
             
@@ -55,30 +67,67 @@ namespace Platform
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            app.UseExceptionHandler("/error.html");
+            if (env.IsProduction())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseHsts();
             }
+
+            app.UseHttpsRedirection();
+            app.UseStatusCodePages("text/html", ResponseSetting.DefaultResponse);
 
             app.UseCookiePolicy();
             app.UseMiddleware<ConsentMiddleware>();
             app.UseSession();
+            app.UseStaticFiles();
             app.UseRouting();
+
+            app.Use(async (context, next) =>
+            {
+                if(context.Request.Path == "/error")
+                {
+                    context.Response.StatusCode = StatusCodes.Status404NotFound;
+                    await Task.CompletedTask;
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
+            app.Run(context =>
+            {
+                throw new Exception("A test exception");
+            });
+
+            app.Use(async (context, next) => 
+            {
+                await context.Response.WriteAsync($"HTTPS request? {context.Request.IsHttps}");
+                if (context.Request.IsHttps)
+                {
+                    await context.Response.WriteAsync("\nThis is a response to HTTPS request");
+                }
+                await next();
+            });
 
             
             app.UseEndpoints(endpoint =>
             {
 
-                endpoint.MapGet("/cookies", async context =>
-                {
-                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
-                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
-                    context.Session.SetInt32("counter1", counter1);
-                    context.Session.SetInt32("counter2", counter2);
-                    await context.Session.CommitAsync();
-                    await context.Response
-                    .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-                });
+                //endpoint.MapGet("/cookies", async context =>
+                //{
+                //    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+                //    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+                //    context.Session.SetInt32("counter1", counter1);
+                //    context.Session.SetInt32("counter2", counter2);
+                //    await context.Session.CommitAsync();
+                //    await context.Response
+                //    .WriteAsync($"\nCounter1: {counter1}, Counter2: {counter2}");
+                //});
 
                 //endpoint.MapGet("/cookies", async context =>
                 //{
@@ -101,7 +150,7 @@ namespace Platform
 
                 endpoint.MapFallback(async context =>
                 {
-                    await context.Response.WriteAsync("Hello world");
+                    await context.Response.WriteAsync("\nHello world");
                 });
             });
             //app.UseMiddleware<LocationMiddleware>();
