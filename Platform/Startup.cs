b0d5a031a8490d63a551194de7b8141f60a6671e
Narek Khachikian <narekbarev@hotmail.com>
2020-07-23 16:14:03 +0400

    End of Sessioning, Beginning of the HTTPS

--- Platform/Startup.cs
+++ Platform/Startup.cs
@@ -19,17 +19,29 @@ namespace Platform
     public class Startup
     {
 
-        private IConfiguration Config;
+        //private IConfiguration Config;
 
-        public Startup(IConfiguration configuration)
-        {
-            Config = configuration;
-        }
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Config = configuration;
+        //}
 
         
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<MessageOptions>(Config.GetSection("Location"));
+
+            services.Configure<CookiePolicyOptions>(opt =>
+            { opt.CheckConsentNeeded = context => true;});
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(option =>
+            {
+                option.IdleTimeout = TimeSpan.FromMinutes(30);
+                option.Cookie.IsEssential = true;
+            });
+
+            //services.Configure<MessageOptions>(Config.GetSection("Location"));
             
             
             //services.Configure<MessageOptions>(opt => opt.CityName = "Moscow");
@@ -41,27 +53,68 @@ namespace Platform
         }
 
         
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseStaticFiles(new StaticFileOptions() { RequestPath = "/file" });
+            app.UseCookiePolicy();
+            app.UseMiddleware<ConsentMiddleware>();
+            app.UseSession();
             app.UseRouting();
 
-            //app.UseMiddleware<LocationMiddleware>();
-
+            
             app.UseEndpoints(endpoint =>
             {
-                endpoint.MapGet("/", async context =>
+
+                endpoint.MapGet("/cookies", async context =>
                 {
-                    logger.LogDebug("Response for / started");
-                    await context.Response.WriteAsync("Hello World");
-                    logger.LogDebug("Response for / finished");
+                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+                    context.Session.SetInt32("counter1", counter1);
+                    context.Session.SetInt32("counter2", counter2);
+                    await context.Session.CommitAsync();
+                    await context.Response
+                    .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
+                });
+
+                //endpoint.MapGet("/cookies", async context =>
+                //{
+                //    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
+                //    context.Response.Cookies.Append("counter1", counter1.ToString(),
+                //        new CookieOptions() { MaxAge = TimeSpan.FromMinutes(30), IsEssential = true });
+                //    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
+                //    context.Response.Cookies.Append("counter2", counter2.ToString(),
+                //        new CookieOptions() { MaxAge = TimeSpan.FromMinutes(30) });
+                //    await context.Response.WriteAsync($"Counter1 = {counter1}, Counter2 = {counter2}");
+                //});
+
+                //endpoint.MapGet("clear", context =>
+                //{
+                //    context.Response.Cookies.Delete("counter1");
+                //    context.Response.Cookies.Delete("counter2");
+                //    context.Response.Redirect("/");
+                //    return Task.CompletedTask;
+                //});
+
+                endpoint.MapFallback(async context =>
+                {
+                    await context.Response.WriteAsync("Hello world");
                 });
             });
+            //app.UseMiddleware<LocationMiddleware>();
+
+            //app.UseEndpoints(endpoint =>
+            //{
+            //    endpoint.MapGet("/", async context =>
+            //    {
+            //        logger.LogDebug("Response for / started");
+            //        await context.Response.WriteAsync("Hello World");
+            //        logger.LogDebug("Response for / finished");
+            //    });
+            //});
 
             //app.Use(async (context, next) =>
             //{
