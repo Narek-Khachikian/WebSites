6f192348793a7c21e8c2e44b4dcb340780099817
Narek Khachikian <narekbarev@hotmail.com>
2020-08-04 20:01:57 +0400

    End of view components

---
+++ WebApp/Components/CitySummery.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Components
+{
+    public class CitySummery : ViewComponent
+    {
+        private CityData cityData;
+
+        public CitySummery(CityData ctx)
+        {
+            cityData = ctx;
+        }
+
+        public IViewComponentResult Invoke(string themName)
+        {
+            ViewBag.themName = themName;
+            return View(new CityVewModel()
+            {
+                Cities = cityData.Cities.Count(),
+                Population = cityData.Cities.Sum(c=>c.Population)
+            });
+            
+        }
+    }
+}
