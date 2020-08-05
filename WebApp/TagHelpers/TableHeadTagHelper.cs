1f0c5667121f2fb23b2b16eebb15382d20148edf
Narek Khachikian <narekbarev@hotmail.com>
2020-08-05 13:31:22 +0400

    Built in tag helpers

---
+++ WebApp/TagHelpers/TableHeadTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+
+namespace WebApp.TagHelpers
+{
+    [HtmlTargetElement("tablehead")]
+    public class TableHeadTagHelper : TagHelper
+    {
+        public string BgColor { get; set; } = "light";
+        public string TextColor { get; set; } = "dark";
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "thead";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("class", $"bg-{BgColor} text-{TextColor} text-center");
+            string content = (await output.GetChildContentAsync()).GetContent();
+            output.Content
+            .SetHtmlContent($"<tr><th colspan=\"2\">{content}</th></tr>");
+        }
+    }
+}
