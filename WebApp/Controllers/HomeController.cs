7e36973b655c75cfacd97e18b981cc6d30f8946c
Narek Khachikian <narekbarev@hotmail.com>
2020-08-18 10:29:12 +0400

    Beginning of CRUD operations

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -5,51 +5,26 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using WebApp.Filters;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
-    [HttpsOnly]
+    [AutoValidateAntiforgeryToken]
     public class HomeController : Controller
     {
-        [ServiceFilter(typeof(GuidResponseAttribute))]
-        [ServiceFilter(typeof(GuidResponseAttribute))]
+        private DataContext dbContext;
+        public HomeController(DataContext ctx)
+        {
+            dbContext = ctx;
+        }
+
+        private IEnumerable<Category> Categories => dbContext.Categories;
+        private IEnumerable<Product> Products => dbContext.Products;
+
         public IActionResult Index()
         {
-            return View("Message", "This is the Index action on Home controller");
-        }
-
-        
-        public IActionResult Secure()
-        {
-            return View("Message", "This is the Secure action on Home controller");
-            
-        }
-
-        [ChangeArg]
-        public IActionResult Message(string message1, string message2 = "None")
-        {
-            Dictionary<string, string> dictionary = new Dictionary<string, string>();
-            dictionary.Add("message1", message1);
-            dictionary.Add("message2", message2);
-
-            return View("Message", dictionary);
-        }
-
-        [RangeException]
-        public ViewResult GenerateException(int? id)
-        {
-            if(id == null)
-            {
-                throw new ArgumentOutOfRangeException(nameof(id));
-            }
-            else if(id > 10)
-            {
-                throw new ArgumentOutOfRangeException(nameof(id));
-            }
-            else
-            {
-                return View("Message", $"The vlaue is {id}");
-            }
+            return View(dbContext.Products.Include(p => p.Category).Include(p => p.Supplier));
         }
     }
 }
--- WebApp/Models/Product.cs
+++ WebApp/Models/Product.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace WebApp.Models
 {
@@ -12,18 +13,22 @@ namespace WebApp.Models
     {
         public long ProductId { get; set; }
 
+        [Required]
+        [Display(Name="Name")]
         public string Name { get; set; }
 
         [Column(TypeName = "decimal(8, 2)")]
+        [Required(ErrorMessage ="Insert a price")]
+        [Range(0.01,9_999_999)]
         //[DisplayFormat(DataFormatString ="{0:c2}", ApplyFormatInEditMode =true)]
         public decimal Price { get; set; }
 
-        [Remote("CategoryKey","Validation",ErrorMessage ="Enter an existing category Id")]
+        
         public long CategoryId { get; set; }
 
         public Category Category { get; set; }
 
-        [Remote("SupplierKey","Validation", ErrorMessage ="Enter an existing supplier Id")]
+        
         public long SupplierId { get; set; }
 
         public Supplier Supplier { get; set; }
