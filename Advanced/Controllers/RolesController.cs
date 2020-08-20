79a898992d1415586c5a34b07a179179c0b5dfcc
Narek Khachikian <narekbarev@hotmail.com>
2020-08-20 16:45:54 +0400

    Ready for Authenticating Users

---
+++ Advanced/Controllers/RolesController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Advanced.Models.ViewModels;
+
+namespace Advanced.Controllers
+{
+    public class RolesController : Controller
+    {
+        public RoleManager<IdentityRole> RoleManager { get; set; }
+        public UserManager<IdentityUser> UserManager { get; set; }
+
+        public RolesController(RoleManager<IdentityRole> roleM, UserManager<IdentityUser> userM)
+        {
+            RoleManager = roleM;
+            UserManager = userM;
+        }
+
+        public async Task<IActionResult> RoleList()
+        {
+            RolesViewModel model = new RolesViewModel()
+            {
+                Roles = RoleManager.Roles
+            };
+            foreach (IdentityRole role in model.Roles)
+            {
+                IList<IdentityUser> list = await UserManager.GetUsersInRoleAsync(role.Name);
+                string names = String.Join(", ", list);
+                model.RoleMembers.Add(role.Name,names);
+            }
+            return View(model);
+        }
+
+        [HttpPost,AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> DeleteRole([FromForm] string id)
+        {
+            IdentityResult result = await RoleManager.DeleteAsync(await RoleManager.FindByIdAsync(id));
+            return RedirectToAction(nameof(RoleList));
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost,AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Create([FromForm] RolesViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await RoleManager.CreateAsync(viewModel.Role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(RoleList));
+                }
+                foreach(IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Editor(string id)
+        {
+            if(id.Length > 0)
+            {
+                IdentityRole role = await RoleManager.FindByIdAsync(id);
+                IList<IdentityUser> users = await UserManager.GetUsersInRoleAsync(role.Name);
+                IList<IdentityUser> nonusers = UserManager.Users.ToList().Except(users).ToList();
+                RolesViewModel model = new RolesViewModel()
+                {
+                    Role = role,
+                    Members = users,
+                    nonMembers = nonusers
+                };
+
+                return View(model);
+            }
+            return RedirectToAction(nameof(RoleList));
+        }
+
+        [HttpPost,AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Editor([FromForm] string roleName,[FromQuery] string userid)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await UserManager.FindByIdAsync(userid);
+                bool member = await UserManager.IsInRoleAsync(user, roleName);
+                IdentityResult result;
+                if (member)
+                {
+                    result = await UserManager.RemoveFromRoleAsync(user, roleName);
+                }
+                else
+                {
+                    result = await UserManager.AddToRoleAsync(user, roleName);
+                }
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(RoleList));
+                }
+                foreach(IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return RedirectToAction(nameof(Editor),new { id = RoleManager.FindByNameAsync(roleName).Id });
+        }
+    }
+}
---
+++ Advanced/Models/ViewModels/RolesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advanced.Models.ViewModels
+{
+    public class RolesViewModel
+    {
+        public IEnumerable<IdentityRole> Roles { get; set; }
+        public Dictionary<string, string> RoleMembers { get; set; } = new Dictionary<string, string>();
+        public IList<IdentityUser> Members { get; set; }
+        public IList<IdentityUser> nonMembers { get; set; }
+        public IdentityRole Role { get; set; }
+    }
+}
